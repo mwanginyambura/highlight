17eecf1b559e72037ecf4dc82a44895814896770
Nyambura Mwangi <mwanginyambura19@gmail.com>
2019-04-17 10:55:03 +0300

    update the file

---
+++ app/main/views.py
@@ -0,0 +1,24 @@
+from flask import render_template,request,redirect,url_for
+from . import main
+from ..requests import get_news
+
+
+
+#Views
+@main.route('/')
+def index():
+    
+ 
+        return render_template('index.html')
+
+@main.route('/news/<int:id>')
+def news(id):
+
+    '''
+    View news page function that returns the news details page and its data
+    '''
+    news = get_news(id)
+    title = f'{news.title}'
+
+    return render_template('news.html',title = title,news = news)
+
